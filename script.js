2a9112f92f61e9a2f250157b848f81e1958359bf
Rajvardhan Pawar <99598319+RajvardhanP@users.noreply.github.com>
2024-02-02 19:54:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $('.container').mouseenter(function () {
+        $('.card').stop().animate({
+            top: '-90px'
+        }, 'slow');
+    }).mouseleave(function () {
+        $('.card').stop().animate({
+            top: 0
+        }, 'slow');
+    });
+});
+$(".container").click(function(){
+    var sound = new Audio('sound.mp3');
+    sound.play();
+})
